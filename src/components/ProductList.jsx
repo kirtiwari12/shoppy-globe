430e18549e1211c04a1007f94426a932a941324f
kirtiwari12 <kirtiwari22@gmail.com>
2025-07-21 03:54:34 +0530

    adjust spacing

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,7 +5,7 @@ export const ProductList = () => {
   const products = useGetAllProducts();
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-5">
       {products?.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}

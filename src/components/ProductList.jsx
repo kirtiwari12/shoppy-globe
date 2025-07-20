8be8c8aa2621f0e48c35aa79f77cb7f45f4b9de0
kirtiwari12 <kirtiwari22@gmail.com>
2025-07-21 04:52:23 +0530

    add pagination

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,13 +2,24 @@ import { ProductItem } from "./ProductItem";
 import { useGetAllProducts } from "../hooks/useGetAllProducts";
 
 export const ProductList = () => {
-  const products = useGetAllProducts();
+  const { products, hasMoreProducts, fetchNextPage, isLoading } =
+    useGetAllProducts();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-5">
-      {products?.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
+    <div className="my-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-5">
+        {products?.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+      {hasMoreProducts && !isLoading && (
+        <button
+          onClick={fetchNextPage}
+          className="justify-self-center flex bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors cursor-pointer"
+        >
+          Load More Products
+        </button>
+      )}
     </div>
   );
 };
--- src/hooks/useGetAllProducts.js
+++ src/hooks/useGetAllProducts.js
@@ -1,17 +1,47 @@
 import { useState, useEffect } from "react";
 
+const pageSize = 20;
+
 export const useGetAllProducts = () => {
-  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [productsMap, setProductsMap] = useState({});
+  const [hasMoreProducts, setHasMoreProducts] = useState(true);
+
+  const products = Object.values(productsMap).flat();
+
+  const [paginationParams, setPaginationParams] = useState({
+    limit: pageSize,
+    skip: 0,
+  });
+
+  function fetchNextPage() {
+    setPaginationParams((prev) => ({
+      ...prev,
+      skip: prev.skip + pageSize,
+    }));
+  }
 
   useEffect(() => {
     try {
-      fetch("https://dummyjson.com/products")
+      setIsLoading(true);
+
+      const { limit, skip } = paginationParams;
+      const query = new URLSearchParams(paginationParams).toString();
+      const url = `https://dummyjson.com/products?${query}`;
+
+      fetch(url)
         .then((res) => res.json())
-        .then((data) => setProducts(data.products));
+        .then((data) => {
+          const newProducts = data.products;
+          setProductsMap((prev) => ({ ...prev, [query]: newProducts }));
+          setHasMoreProducts(limit + skip < data.total);
+        });
     } catch (error) {
       console.error("Error fetching products:", error);
+    } finally {
+      setIsLoading(false);
     }
-  }, []);
+  }, [paginationParams]);
 
-  return products;
+  return { products, isLoading, hasMoreProducts, fetchNextPage };
 };

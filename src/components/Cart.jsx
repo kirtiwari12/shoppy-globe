6ce06d954ce38385f78b78275f42f33f1291f07f
kirtiwari12 <kirtiwari22@gmail.com>
2025-08-17 15:09:50 +0530

    add go to home button in empty cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import { CartItem } from "./CartItem";
 import { useCart, useCartTotal } from "../store/slices/cart";
+import { Link } from "react-router";
 
 export const Cart = () => {
   const cart = useCart();
@@ -10,7 +11,14 @@ export const Cart = () => {
       <h1 className="text-2xl font-bold">Cart</h1>
       <div className="max-w-2xl mx-auto my-5">
         {cart.length === 0 ? (
-          <div className="text-center text-gray-500">Your cart is empty.</div>
+          <>
+            <div className="text-center text-gray-500">Your cart is empty.</div>
+            <Link to="/">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-md mx-auto block my-4 cursor-pointer">
+                Go to Home
+              </button>
+            </Link>
+          </>
         ) : (
           <>
             {cart.map((product) => (
